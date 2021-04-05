984881fa8b5163d8699bd6427de0e1086fca447d
Abhishek Cherath <abhicherath@gmail.com>
2021-04-05 14:10:19 -0400

    docs updated

--- src/constants.ts
+++ src/constants.ts
@@ -2,10 +2,19 @@
 // this thing needs to be a function cause smartweave might not be defined
 // sometimes.
 
-/* eslint-disable @typescript-eslint/prefer-literal-enum-member */
+/* eslint-disable new-cap */
 
 declare const SmartWeave: any;
-enum Constants {
-	A = 1,
-	END_BLOCK = Number(SmartWeave.block.height) + 1000
-}
+export const START_BLOCK = () => Number(SmartWeave.block.height);
+export const END_BLOCK = () => START_BLOCK() + 1000;
+export const MIN_BAL_FOR_PROPOSAL = 0;
+export const MIN_BAL_FOR_BIDDING = 0;
+export const MIN_BAL_FOR_STAKING = 0;
+export const STAKE_FOR_BIDDING = (bid_amount: number) =>
+	0.01 * bid_amount;
+export const TRANSACTION_FEE = (transaction_amount: number) =>
+	0.1 * transaction_amount;
+export const NUMBER_OF_VALIDATORS_PER_VALIDATION = 2;
+export const VALIDATOR_REWARD = (bid_amount: number) =>
+	TRANSACTION_FEE(bid_amount) / NUMBER_OF_VALIDATORS_PER_VALIDATION;
+// Fees specified as parts out of 1.
---
+++ src/swglobal/ACKNOWLEDGEMENT.md
@@ -0,0 +1,4 @@
+Code adapted (*changes indicated in resp. files*) and partly vendored 
+from community.xyz smart contract swglobal smartweave testing harness taken from 
+[link to communityjs repo](https://github.com/CommunityXYZ/contract/tree/master/src/swglobal)
+as of commit hash b07a9ea, 25th July 2020.
